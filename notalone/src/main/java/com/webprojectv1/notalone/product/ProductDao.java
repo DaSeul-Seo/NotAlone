a00ade64389a6d257d03529e97a26204a5ff2014
star.dss999@gmail.com <star.dss999@gmail.com>
2023-12-15 15:11:15 +0900

    create dao & service file

---
+++ notalone/src/main/java/com/webprojectv1/notalone/product/ProductDao.java
@@ -0,0 +1,13 @@
+package com.webprojectv1.notalone.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import groovy.util.logging.Slf4j;
+
+@Slf4j
+@Service
+public class ProductDao {
+    @Autowired
+    private IProductRepository productRepository;
+}
