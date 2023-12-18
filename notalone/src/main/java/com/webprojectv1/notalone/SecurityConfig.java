73174d45740f6742651059b179589b60381b2d0b
jeongsol <jeongsol@outlook.com>
2023-12-18 11:11:12 +0900

    ﻿로그아웃기능

--- notalone/src/main/java/com/webprojectv1/notalone/SecurityConfig.java
+++ notalone/src/main/java/com/webprojectv1/notalone/SecurityConfig.java
@@ -24,15 +24,36 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/"));
         return http.build();
     }
+=======
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                                                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                                .csrf((csrf) -> csrf
+                                                .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                                .headers((headers) -> headers
+                                                .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                                .formLogin((formLogin) -> formLogin
+                                                .loginPage("/user/login")
+                                                .defaultSuccessUrl("/"))
+                                .logout((logout) -> logout
+                                                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                                                .logoutSuccessUrl("/")
+                                                .invalidateHttpSession(true));
+                return http.build();
+        }
+>>>>>>> 9d92afc (로그아웃기능)
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-            throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+        @Bean
+        AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+                        throws Exception {
+                return authenticationConfiguration.getAuthenticationManager();
+        }
 }
