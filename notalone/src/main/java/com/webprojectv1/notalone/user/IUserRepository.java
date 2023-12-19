bee8634fe5139d4263c323d6c6bac28a25a2abc1
jeongsol <jeongsol@outlook.com>
2023-12-19 15:13:22 +0900

    로그인 기능 재구성

--- notalone/src/main/java/com/webprojectv1/notalone/user/IUserRepository.java
+++ notalone/src/main/java/com/webprojectv1/notalone/user/IUserRepository.java
@@ -4,6 +4,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<SiteUser, Long> {
-    Optional<SiteUser> findByuserName(String username);
+    Optional<SiteUser> findByUserId(String userId);
 
 }
